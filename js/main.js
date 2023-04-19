5c5cc95683e348025d4139c4e310e38af7f895ec
8th <g90g01z0.m08@gmail.com>
2023-04-19 23:06:12 +0900

    debug

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ class App{
         this.outLineFile = new OutlineFile();
         console.log("outlinefile loaded.");
         this.docFrame = new DocFrame(this.outLineFile);
-        console.log("docframe loaded.")
         this.wholeView = new WholeView(this.docFrame);
     }
 }
